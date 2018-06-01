725829a1c52aae1bad510b6b71ef4cac0f0aadc7
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-01 07:44:24 -0700

    made search page called index, renamed previous index results.html, created js file for index

---
+++ assets/javascript/searchScript.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    //once button is submitted, artist variable will be created and 
+    var queryURL; 
+    var selectedLocation; 
+
+    //will display the venues that the artist will play on the search page 
+    function venueSelect(artist){
+        
+
+    }
+
+
+   $("#submit").on("click", function(){
+        //var declaration 
+        var artist = $("#search-term-id").val().trim();
+        console.log(artist);
+        //will display 
+        venueSelect(artist);
+   });
+    
+    }); //end of app.js
+    
