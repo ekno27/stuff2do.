38884844c5e262d9e79ae3f747dd3d0539e5fcce
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-02 19:19:18 -0700

    deleted extra files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
 console.log("hello");
 
+
+
+
+
+
+
+//submit button functionality
+$("#submit").on("click", function(){
+if($("#artist-name").val().trim()==="" || $("#venue-name").val().trim()==="" ){
+    alert("Please fill all categories!");
+}
+
+else{
+//var declaration 
+artist = $("#artist-name").val().trim();
+
+//will display 
+venue = $("#venue-name").val().trim();
+console.log("artist: " + artist);
+console.log("venue: " + venue);
+
+}
+
+});
+
+
 }); //end of app.js
